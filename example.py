09254c750431da0ffa252ca913b0d51022dcf4f6
RSchueder <rudy.schueder@outlook.com>
2019-08-17 20:26:36 +0200

    added .gitignore

--- example.py
+++ example.py
@@ -5,11 +5,11 @@ Created on Fri Aug  9 09:27:28 2019
 @author: schueder
 """
 
+import datetime
+import os
 import tide
 import query
 import model
-import datetime
-import os
 
 ###############################################################################
 # DATA QUERY
@@ -21,12 +21,12 @@ time_vect  = {'t_start' : '2017-05-15 12:00:00',
 dataset    = 'bio'
 
 out = os.path.join(os.getcwd(), 'tests', 'Med' , 'out')
-coords     = [0, 2, 39, 42]
+coords      = [0, 2, 39, 42]
 credentials = 'credentials.json'
 
 med_query = query.Query(time_vect, dataset, coords, credentials, out)
 med_query.build_query()
-#med_query.send_request_linux()
+med_query.send_request_linux()
 
 ###############################################################################
 # TIDE BOUNDARY
@@ -36,18 +36,18 @@ fes_path = os.path.join('p','1206126-nevref','Maialen','DATA','fromCornelis','FE
 pli      =  os.path.join(os.getcwd(), 'tests', 'Med' , 'in', 'Boundary01.pli')
 out      = os.path.join(os.getcwd(), 'tests', 'Med' , 'out')
 med_tide = tide.Tide(fes_path, coords, pli, out)
-#med_tide.initiate_tide()
+med_tide.initiate_tide()
 
 ###############################################################################
 # CONSTITUENT BOUNDARY
 ###############################################################################
 
-#ext = med_tide.ext
+ext = med_tide.ext
 ext = os.path.join(os.getcwd(), 'tests', 'Med' , 'out', 'Boundary01.ext')
 data_list = os.path.join(os.getcwd(), 'tests', 'Med' , 'out', 'data', '*.nc')
 
-sub       = ['salinity', 'temperature', 'uxuy', 'steric']
-tref      = datetime.datetime(2000,1,1,00,00,00)
-model_dir = med_tide.out
+sub        = ['salinity', 'temperature', 'uxuy', 'steric']
+tref       = datetime.datetime(2000,1,1,00,00,00)
+model_dir  = med_tide.out
 med_mod    = model.Model(ext, data_list, sub, tref, model_dir)
 med_mod.build_boundary(interp = True, simultaneous = True)
